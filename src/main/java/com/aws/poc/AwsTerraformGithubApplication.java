19aecbce0135c7037180f64291bf7cd8e1b7e4b2
SujiDev-16 <sujithasugumar11@gmail.com>
2022-07-07 20:02:05 +0530

    pushing second

---
+++ src/main/java/com/aws/poc/AwsTerraformGithubApplication.java
@@ -0,0 +1,13 @@
+package com.aws.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTerraformGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTerraformGithubApplication.class, args);
+	}
+
+}
